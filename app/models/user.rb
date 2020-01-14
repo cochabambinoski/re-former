527dd7af3fed985a40ee587a6b9479c146c74a92
OlukaDenis <olukadeno@gmail.com>
2020-01-15 02:37:14 +0300

    Implemented the form_for

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
     validates :email, presence: true,
                       format: {with: VALID_EMAIL_REGEX}
     validates :password, presence: true
-    has_secure_password
 end
