54adfd66f6aab031dbec1ea204d1eeec9f2dd502
Feber <cochabambinoski@gmail.com>
2020-01-14 17:51:49 -0400

    Fixing DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
     def new
-        
+        @user = User.new
     end
 
     def create
-        
-    end
+        @user = User.new(user_params)
+        puts @user
+        if @user.save
+          redirect_to new_user_path
+        else
+          puts @user.errors.messages
+          render :new
+        end
+      end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_175525) do
+ActiveRecord::Schema.define(version: 2020_01_14_174555) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
