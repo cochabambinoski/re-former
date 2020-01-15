42be299658bff2cc11100b642f511d466c63b994
Feber <cochabambinoski@gmail.com>
2020-01-15 09:51:32 -0400

    Adding linterns and complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    puts @user
+    if @user.save
+      redirect_to new_user_path
+    else
+      puts @user.errors.messages
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        puts @user
-        if @user.save
-          redirect_to new_user_path
-        else
-          puts @user.errors.messages
-          render :new
-        end
-      end
+  private
 
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :username, presence: true
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true,
-                      format: {with: VALID_EMAIL_REGEX}
-    validates :password, presence: true
+  validates :username, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
 end
